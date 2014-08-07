12f05384522da22723a12de9d67838e36678d382
dbortnichuk <dbortnichuk@gmail.com>
2014-08-07 07:50:24 +0300

    stanford_algorithms1_pq1_InversionsCounter - completed for nlogn

--- algorithms_1/src/main/java/com/dbortnichuk/stanford/algorithms/pq1/InversionsCounter.java
+++ algorithms_1/src/main/java/com/dbortnichuk/stanford/algorithms/pq1/InversionsCounter.java
@@ -1,5 +1,8 @@
 package com.dbortnichuk.stanford.algorithms.pq1;
 
+import com.dbortnichuk.exception.PerformanceEvaluatorException;
+import com.dbortnichuk.utils.PerformanceEvaluator;
+
 import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
@@ -46,9 +49,70 @@ public class InversionsCounter {
         return data;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws PerformanceEvaluatorException {
+        PerformanceEvaluator performanceEvaluator = PerformanceEvaluator.newInstance();
+
         InversionsCounter inversionsCounter = new InversionsCounter("/IntegerArray.txt");
         Integer[] inputData = inversionsCounter.getData().toArray(new Integer[]{});
+
+        performanceEvaluator.start();
+        long inversionCount = inversionsCounter.getInversionsOnlogn(inputData, 0, inputData.length - 1);
+        //long inversionCount = inversionsCounter.getInversionsOn2(inputData);
+        performanceEvaluator.stop();
+
+        System.out.println("Inversions count: " + inversionCount);
     }
+
+    public long getInversionsOn2(Integer[] array) {
+        long inversionsCount = 0;
+        int arrayLength = array.length;
+        for (int i = 0; i < arrayLength - 1; i++) {
+            for (int j = i + 1; j < arrayLength; j++) {
+                if (array[i] > array[j]) {
+                    inversionsCount++;
+                }
+            }
+        }
+        return inversionsCount;
+    }
+
+    public long getInversionsOnlogn(Integer[] array, int start, int end) {
+        long inversionsCount = 0;
+        if (start < end) {
+            int median = (start + end) / 2;
+            long leftInversionsCount = getInversionsOnlogn(array, start, median);
+            long rightInversionsCount = getInversionsOnlogn(array, median + 1, end);
+            long mergeInversionsCount = getMergeInversions(array, start, median, end);
+            inversionsCount = leftInversionsCount + rightInversionsCount + mergeInversionsCount;
+        }
+        return inversionsCount;
+    }
+
+    private long getMergeInversions(Integer[] array, int start, int median, int end) {
+        int mergeInversionsCount = 0;
+
+        int leftLength = median - start + 1;
+        int rightLength = end - median;
+        Integer[] leftHalf = new Integer[leftLength + 1];
+        Integer[] rightHalf = new Integer[rightLength + 1];
+        for (int i = 0; i < leftLength; i++) leftHalf[i] = array[start + i];
+        for (int i = 0; i < rightLength; i++) rightHalf[i] = array[median + 1 + i];
+
+        leftHalf[leftLength] = Integer.MAX_VALUE;
+        rightHalf[rightLength] = Integer.MAX_VALUE;
+        int i = 0, j = 0;
+        for (int k = start; k <= end; k++) {
+            if (leftHalf[i] <= rightHalf[j]) {
+                array[k] = leftHalf[i];
+                i++;
+            } else {
+                array[k] = rightHalf[j];
+                j++;
+                mergeInversionsCount = mergeInversionsCount + (leftLength - i);
+            }
+        }
+        return mergeInversionsCount;
+    }
+
 
 }
